eea220bf4a6fcec068f75b76e67ea35a81a7390f
Ivan <ivaz3823@gmail.com>
2020-12-19 20:53:24 +0300

    Strategy implemented

--- core/duplchecker.cpp
+++ core/duplchecker.cpp
@@ -7,4 +7,12 @@ DuplChecker::DuplChecker()
 {
     setDuplicateChackerAlg();
 }
+
+QList<FilesPair> DuplChecker::getDuplicationFilesPaths(const QList<QFile>& files1,
+                                                      const QList<QFile>& files2)const
+{
+    if (!m_duplChecker) return QList<FilesPair>();
+
+    return m_duplChecker->getDuplicationFilesPaths(files1, files2);
+}
 }
